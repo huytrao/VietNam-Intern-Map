ce02665931038d7f892c8f245474efe759f436f1
Huy Trao <121539558+huytrao@users.noreply.github.com>
2024-01-02 08:31:50 +0000

    demo_1

--- myapp.py
+++ myapp.py
@@ -1,15 +1,19 @@
-import streamlit as st  # khai báo thư viện
+import streamlit as st
+import leafmap.kepler as leafmap
 import pandas as pd
-import numpy as np
-import pandas as pd
-# hàm nhập tên biến ở việt nam
-ten = st.text_input("Nhập tên ngành nghề bạn muốn đi thực tập ở việt nam")
 
-df = pd.DataFrame()
-data = { 
-        'name'
-        'lat': [-73.96727182919946],
-        'lon': [40.750214150000005]}
-df = pd.DataFrame(data)
-df
-st.map(df)
+#hiển thị độ rộng của bản đồ 
+st.set_page_config(layout="wide")
+df = pd.read_csv("./data/job_ex.csv")
+st.dataframe(df)
+st.title("leafmap streamlit demo")
+job = st.text_input("Job Intern you want to find:")  # Use st.text_input to get user input
+
+"## Create a 3D map using Kepler.gl"
+# # ghim map ở thành phố hồ chí minh
+m = leafmap.Map(center=[10.785160732570825, 106.69416733524045], zoom=1)
+# m = leafmap.Map(center=[37.7621, -122.4143], zoom=12)
+in_csv = "./data/job_ex.csv"
+config = "./configmaps/hex_config.json"
+m.add_csv(in_csv, layer_name="job_data", config=config)
+m.to_streamlit()
